917f4c0d513fd96f5b79cff837ac5536470847d5
Yurii Boltukov <mr.kolomiha@bk.ru>
2021-01-02 14:53:12 +0300

    init: start DTS project

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { FeaturesComponent } from './features/features.component';
+import { CharacteristicsComponent } from './characteristics/characteristics.component';
+import { QuestionsComponent } from './questions/questions.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AboutComponent,
+    FeaturesComponent,
+    CharacteristicsComponent,
+    QuestionsComponent,
+    ContactsComponent,
+    HomeComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
